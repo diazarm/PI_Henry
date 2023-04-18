dee556aee43e7975a561dd9ccf35ae7a7529eb4b
diazarm <marceloardiaz@gmail.com>
2023-04-18 10:53:59 -0300

    subiendo pruebas

--- api/src/db.js
+++ api/src/db.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
+//probando git
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
 const {
-  DB_USER, DB_PASSWORD, DB_HOST,
+  DB_USER, DB_PASSWORD, DB_HOST, 
 } = process.env;
 
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
