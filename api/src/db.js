d9b938173a99478d953bdfcaa6227530de3e7aa0
diazarm <marceloardiaz@gmail.com>
2023-04-18 11:37:25 -0300

    configuraciones back

--- api/src/db.js
+++ api/src/db.js
@@ -1,15 +1,12 @@
 require('dotenv').config();
-//probando git
-const { Sequelize } = require('sequelize');
+const { Sequelize, DataTypes } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const {
-  DB_USER, DB_PASSWORD, DB_HOST, 
-} = process.env;
+const {DB_USER, DB_PASSWORD, DB_HOST,DB_NAME} = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/dogs`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+  logging: false, // No muestra en consola cuando ejecuta un comando.
+  native: false, // No utiliza el controlar nativo de pg.
 });
 const basename = path.basename(__filename);
 
