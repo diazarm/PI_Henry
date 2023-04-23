a9921dc586a75e87a3dc7c805c0312f1f4ee85dc
diazarm <marceloardiaz@gmail.com>
2023-04-23 13:48:59 -0300

    Back finalizado

--- api/src/controllers/getController.js
+++ api/src/controllers/getController.js
@@ -4,19 +4,21 @@ const {API_KEY} = process.env;
 
 const LinkApi = `https://api.thedogapi.com/v1/breeds/?api_key=${API_KEY}`;
 
+
+//muestra el perro de la api de a cuerdo a las caract
 const raceDogApi = async () =>{
     const raceDog =  (await axios.get(LinkApi)).data;
-    return raceDog.map(race => ({id:race.id, image:race.image, name:race.name, height:race.height, weight: race.weight, life_span: race.life_span  }))
-    
-}
-//este funciona pero devuelve todo
-// const getUserById = async(id, source) => {
-//    const dogId = source === "api"
-//     ?(await axios.get(`https://api.thedogapi.com/v1/breeds/${id}/?api_key=${API_KEY}`)).data
-//     : await Dogs.findByPk(id);
-//     return dogId; 
-// }
+    return raceDog.map(race => (
+        {id:race.id, 
+        image:race.image.url, 
+        name:race.name, 
+        height:race.height.metric, 
+        weight: race.weight.metric, 
+        life_span: race.life_span,
+        created:false  }));
+};
 
+//muestra de acuerdo al id en api y bdd
 const getUserById = async (id, source) => {
   const urlData = (await axios.get(LinkApi)).data;
   if (source === "api") {
@@ -36,60 +38,40 @@ const getUserById = async (id, source) => {
   }
 };
 
+// busca por nombre api y bdd
+const searchDogByName = (name)=>{
 
+}
 
-
-
-
-
-
-
-
-
-
+//crea nuevo perro en dbb
 const createNewDog = async(name, height, weight, life_span,image, created) =>{
     if (name && height && weight && life_span &&image) {
         return await Dogs.create({name, height, weight, life_span,image,created}) //el await es pq devuelve la promesa.
-    }else{ throw new Error('Faltan datos')}
-   
-}
-const searchDogByName = ()=>{
-
-}
+    }else{ throw new Error('Faltan datos')};
+};
 
 
 
-
+//muestra todos los perros de api y bdd 
 const getAllDogs =async() => {
-
     const dataBaseDogs = await Dogs.findAll();
-
-    const apiDogsRaw = await (axios.get(`https://api.thedogapi.com/v1/breeds/?api_key=${API_KEY}`)).data 
-
-    const apiDogs = cleanArray(apiDogsRaw)   
-    
+    const apiDogsRaw = (await axios.get(LinkApi)).data 
+       
     const cleanArray = (arr)=>arr.map((elem)=>{
-            return {
-                id:elem.id,
-                name:elem.name,
-                height:elem.heigt,
-                weight:elem.weight,
-                life_span:elem.life_span,
-                image:elem.image
-            }
-        })
-    return [...dataBaseDogs, ...apiDogs];
-}
-
-const cleanArray = (arr)=>arr.map((elem)=>{
-    return {
+      return {
         id:elem.id,
         name:elem.name,
         height:elem.heigt,
         weight:elem.weight,
         life_span:elem.life_span,
         image:elem.image
-    }
-})
+      };
+   });
+    const apiDogs = cleanArray(apiDogsRaw)   
+   return [...dataBaseDogs, ...apiDogs];
+   
+};
+
+
 
 module.exports = {raceDogApi, getUserById, createNewDog, searchDogByName, getAllDogs};
--- api/src/routes/dogRouter.js
+++ api/src/routes/dogRouter.js
@@ -1,18 +1,21 @@
 const {Router} = require('express');
-const  {getDogRaceHandler, 
-        getIdHandler, 
+const  {getDogRaceHandler,getIdHandler, 
         getNameHandler, postDogHandler
        } = require('../handlers/dogGetHandler');
+const { getAllDogs } = require('../controllers/getController');
 
 const dogRouter = Router();
 
 
 dogRouter.get("/", getDogRaceHandler);
 
-dogRouter.get("/:idRaza", getIdHandler);
-
 dogRouter.get("/name", getNameHandler);
 
+dogRouter.get("/:idRaza", getIdHandler);
+
 dogRouter.post("/", postDogHandler);
+
+
+
 
 module.exports = dogRouter;
