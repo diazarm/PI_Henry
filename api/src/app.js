2fbd24cf4ff376e2c2c6a66b3602f85d17f5fd58
diazarm <marceloardiaz@gmail.com>
2023-04-25 21:29:49 -0300

    ultimas actualizaciones de back

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require ('cors');
 const morgan = require('morgan');
 const mainRouter = require('./routes/mainRouter.js');
 //const cookieParser = require('cookie-parser');
@@ -8,13 +9,13 @@ const mainRouter = require('./routes/mainRouter.js');
 //require('./db.js');
 
 const app = express();
+app.use(cors());
 
 app.use(morgan('dev'));
 
 app.use(express.json());
 
 app.use(mainRouter);
-
 
 
 module.exports = app;
